743fcdc93e1cc98810000a3e6850bc5596793d71
Emma Maenhout <emmamaenhout@Emmas-MacBook-Air.local>
2024-05-22 13:57:44 -0700

    random message added

---
+++ random.js
@@ -0,0 +1,21 @@
+ const signGenerator = () => {
+ const randomNumAstro = Math.floor(Math.random()* 12);
+ const astrology = ["Gemini", "Taurus", "Libra", "Sagittarius", "Cancer", "Aries", "Aquarius", "Capricorn", "Pisces", "Virgo", "Leo", "Scorpio"];
+ // don't forget to put string elements between quotes
+ 
+ console.log(astrology[randomNumAstro]);
+ // this way we can use the randomNumAstro for i and it will correlate with the array
+
+};
+
+//signGenerator(); this was used to call function and check it works
+
+const eventCalculator = () => {
+    const randomNumEvent = Math.floor(Math.random()* 10);
+    const event = ["getting married", "experience love at first sight", "get their dream job", "needing to be cautious", "going to their anual physical exam", "exploring the world", "fighting against lactose intolerance", "win the lottery", "feeling happy", "needing an ABBA disco night"];
+
+    console.log(event[randomNumEvent]);
+    // made a small mistake putting the random between () instead []
+}
+
+eventCalculator();
